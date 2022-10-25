dae41f7caa9a5ce88e226568c70ff77a805470cd
Robert Wilson <rjw11@sfu.ca>
2022-10-24 20:01:58 -0700

    created MovingEntity class

--- Documents/game/Entity.java
+++ Documents/game/Entity.java
@@ -1,10 +1,16 @@
 package game;
+import java.awt.*;
 
 public class Entity {
     Position pos;
 
     public Position getPos() {
         return pos;
+    }
+
+    public void draw(Image picture) {
+        // TO-DO:
+        //   Dependent on implementation of Map class
     }
 }
 
---
+++ Documents/game/MovingEntity.java
@@ -0,0 +1,21 @@
+package game;
+
+public class MovingEntity extends Entity{
+
+    public void move(Position newPos) {
+        pos = newPos;
+
+        if (checkValidMove(newPos)) {
+            // TO-DO:
+            //   Move entity's position on characters 2D array in Map class
+        }
+
+    }
+
+    public boolean checkValidMove(Position newPos) {
+        //  TO-DO:
+        //    if newPos intersects the walls array:  return false
+        //    if newPos intersects a cat in characters array:  trigger catchMouse() and return false(?)
+        return true;
+    }
+}
