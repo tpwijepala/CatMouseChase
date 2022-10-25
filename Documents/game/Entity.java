4e172ec12c8137613982489e715d66b9c675ee74
Robert Wilson <rjw11@sfu.ca>
2022-10-24 19:27:50 -0700

    created entity class

---
+++ Documents/game/Entity.java
@@ -0,0 +1,4 @@
+package game;
+public class Entity {
+    Position pos;
+}
---
+++ Documents/game/Position.java
@@ -0,0 +1,24 @@
+package game;
+public class Position {
+    protected int x;
+    protected int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public void setY(int newY) {
+        y = newY;
+    }
+}
