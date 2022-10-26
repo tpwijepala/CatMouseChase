6a9fcdfba130dc5206ea114fd9c38e2e5fbd4635
Robert Wilson <rjw11@sfu.ca>
2022-10-26 16:44:03 -0700

    added empty Mouse methods for implementation once dependencies are implemented

--- Documents/game/Mouse.java
+++ Documents/game/Mouse.java
@@ -1,16 +1,16 @@
 package game;
 
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class Mouse extends MovingEntity{
     private Image mousePic;  // mousePic can be declared const and given an initial value once a picture is found and included in files
+    // TO-DO: private Score playerScore;
 
     public Mouse(int x, int y) {
         pos = new Position(x, y);
     }
 
+    //  TEMPORARY:
     public static void main(String[] args) {
         Mouse m = new Mouse(1, 1);
 
@@ -19,6 +19,20 @@ public class Mouse extends MovingEntity{
         f.setSize(200, 200);
         f.setVisible(true);
 
+    }
+
+    public void checkFinish() {
+        //  TO-DO:
+        //    if pos == finish position && all rewards collected:
+        //      end game
+    }
+
+    public void collectItem() {
+        //  TO-DO:
+        //    if pos == position of any item:
+        //      get points of the item
+        //      adjust score based on those points
+        //      despawn item
     }
 
 
