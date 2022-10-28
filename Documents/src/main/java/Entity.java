14d51531fcc4f478276083e0ecb21f09cba2b382
Robert Wilson <rjw11@sfu.ca>
2022-10-27 17:09:51 -0700

    updated java files to fit Maven template

---
+++ Documents/src/main/java/Entity.java
@@ -0,0 +1,23 @@
+package main.java;
+import java.awt.*;
+
+public class Entity {
+    Position pos;
+    /*Graphics g;
+
+    public Entity(Graphics g) {
+        this.g = g;
+    }
+*/
+    public Position getPos() {
+        return pos;
+    }
+
+    public void draw(Image picture) {
+        //g.drawRect(pos.x, pos.y, 10, 10);
+        // TO-DO:
+        //   Dependent on implementation of Map class and how the program window is created
+    }
+}
+
+
