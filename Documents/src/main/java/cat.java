83de4487ed714e4ee37c02fe3af80b8363ee8e88
kkramer215 <kkramer215@gmail.com>
2022-10-27 17:41:47 -0700

    Add cat class

---
+++ Documents/src/main/java/cat.java
@@ -0,0 +1,17 @@
+import java.awt.Image;
+
+class Cat extends MovingEntity {
+    Image catPic = new Image();
+
+    public Cat() {
+        super(Position p);
+    }
+
+    public void catchMouse(Position p) {
+
+    }
+
+    private void getBestMove(Position p) {
+
+    }
+}
