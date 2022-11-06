e1a5539fe7200d9b7d009ea929d6a9af536e0c27
epa25 <epa25@sfu.ca>
2022-11-06 15:28:14 -0800

    Javadocs for Window

--- Documents/src/main/java/Window.java
+++ Documents/src/main/java/Window.java
@@ -2,8 +2,20 @@
 
 import javax.swing.JFrame;
 
+/**
+ * This class encapsulates all the settings for creating the Game window
+ * 
+ * @author Ethan
+ */
 public class Window {
 
+    /**
+     * Contains the specifications for the Game window, creates and opens the window
+     * when called.
+     * 
+     * @param title The title of the window
+     * @param game  The game that we are applying these window specifications to
+     */
     public Window(String title, Game game) {
         JFrame frame = new JFrame(title);
         frame.add(game);
@@ -13,4 +25,5 @@ public class Window {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
 }
