1b7830ca42d08fe58fbbff773811fdaebe712a4a
epa25 <epa25@sfu.ca>
2022-11-06 17:13:09 -0800

    Javadoc for Userinput

--- Documents/src/main/java/UserInput.java
+++ Documents/src/main/java/UserInput.java
@@ -3,33 +3,44 @@
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+/*
+ * Class encapsulates the input for movement
+ * 
+ * @author Robert, Thimara
+ */
 class UserInput extends KeyAdapter {
     Mouse player;
 
+    /*
+     * Sets the userinput to the player
+     */
     public UserInput(Mouse player) {
         this.player = player;
     }
 
+    /*
+     * Moves the actor with input from keyboard
+     */
     public void keyPressed(KeyEvent input) {
         int key = input.getKeyCode();
 
-        if (key == KeyEvent.VK_W) {  // Move UP
+        if (key == KeyEvent.VK_W) { // Move UP
             player.move(new Position(player.getPos().getX(), player.getPos().getY() - 1));
         }
 
-        else if (key == KeyEvent.VK_S) {  // Move DOWN
+        else if (key == KeyEvent.VK_S) { // Move DOWN
             player.move(new Position(player.getPos().getX(), player.getPos().getY() + 1));
         }
 
-        else if (key == KeyEvent.VK_A) {  // Move LEFT
+        else if (key == KeyEvent.VK_A) { // Move LEFT
             player.move(new Position(player.getPos().getX() - 1, player.getPos().getY()));
         }
 
-        else if (key == KeyEvent.VK_D) {  // Move RIGHT
+        else if (key == KeyEvent.VK_D) { // Move RIGHT
             player.move(new Position(player.getPos().getX() + 1, player.getPos().getY()));
         }
 
-        //enemy.catchMouse(player.getPos());
+        // enemy.catchMouse(player.getPos());
         player.collectItem();
 
     }
