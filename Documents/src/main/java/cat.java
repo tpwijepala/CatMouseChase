89e4036c58562721f45c6c5b344394108747253b
kkramer215 <kkramer215@gmail.com>
2022-11-16 19:46:48 -0800

    Fix error with cat ending game early once mouse enters vicinity

--- Documents/src/main/java/cat.java
+++ Documents/src/main/java/cat.java
@@ -40,87 +40,24 @@ class Cat extends MovingEntity {
      * @param mousePos  Mouse's current position after key press
      */
     public void startMove(Position mousePos) {
-        //Add tick method here
-
         catchMouse(mousePos);
     }
 
-   
-
-    //NOTE: This is the attempt to implement a Breadth First Search algorithm to get a better move for the cat than the
-    //      move in the above functions
-    /*
-    public void catchMouse(Position mousePosition) {
-        //Acknowledgements: Thanks to Kevin Litvin for the idea and https://www.youtube.com/watch?v=KiCBXu4P-2Y for the explanation :) 
-
-        int depth = 2;
-        int nodesCurrentLayer = 1;
-        int nodesNextLayer = 0;
-        int cPX = getPos().getX();  //Cat's x coordinate on the game map
-        int cPY = getPos().getY();  //Cat's y coordinate on the game map
-        int[][] visitedCells = new int[5][5];
-        int[] previousX = new int[25];  //Used to reconstruct path
-        int[] previousY = new int[25];
-        int[] directionX = {0, 1, 0, -1};
-        int[] directionY = {-1, 0, 1, 0};
-
-        int[][] miniMap = {{Map.isWall(cPX - 2, cPY - 2), Map.isWall(cPX - 1, cPY - 2), Map.isWall(cPX, cPY - 2), Map.isWall(cPX + 1, cPY - 2), Map.isWall(cPX + 2, cPY - 2)},
-                           {Map.isWall(cPX - 2, cPY - 1), Map.isWall(cPX - 1, cPY - 1), Map.isWall(cPX, cPY - 1), Map.isWall(cPX + 1, cPY - 1), Map.isWall(cPX + 2, cPY - 1)},
-                           {Map.isWall(cPX - 2, cPY), Map.isWall(cPX - 1, cPY), Map.isWall(cPX, cPY), Map.isWall(cPX + 1, cPY), Map.isWall(cPX + 2, cPY)},
-                           {Map.isWall(cPX - 2, cPY + 1), Map.isWall(cPX - 1, cPY + 1), Map.isWall(cPX, cPY + 1), Map.isWall(cPX + 1, cPY + 1), Map.isWall(cPX + 2, cPY + 1)},
-                           {Map.isWall(cPX - 2, cPY + 2), Map.isWall(cPX - 1, cPY + 2), Map.isWall(cPX, cPY + 2), Map.isWall(cPX + 1, cPY + 2), Map.isWall(cPX + 2, cPY + 2)}};
-
-        Queue<Integer> moveTrackerX = new LinkedList<>();
-        Queue<Integer> moveTrackerY = new LinkedList<>();
-
-        //Start from the center of the map to line up with cPX and cPY
-        int miniX = 2;
-        int miniY = 2;
-
-        //Add the starting position
-        moveTrackerX.add(miniX);
-        moveTrackerY.add(miniY);
-        visitedCells[miniX][miniY] = 1;
-
-        while (moveTrackerX.size() > 0) {
-            int tempMiniX = moveTrackerX.remove();
-            int tempMiniY = moveTrackerY.remove();
-
-            //Since the cat's position is centered on the miniMap, we need to shift the coordinates
-            int convertedX = cPX - 2 + tempMiniX;
-            int convertedY = cPX - 2 + tempMiniY;
-
-            //Convert mini coordinates to the map's coordinates
-            if (convertedX == mousePosition.getX() && convertedY == mousePosition.getY()) {
-                Game.State = Game.STATE.LOSE;
-                //break;
-            }
-
-            nodesNextLayer = findNodes(miniX, miniY, directionX, directionY, moveTrackerX, moveTrackerY, miniMap);
-            for ()
-            /*nodesCurrentLayer--;
-
-            if (nodesCurrentLayer == 0) {
-                nodesCurrentLayer = nodesNextLayer;
-                depth--;
-            }
-        }
-
-        //nextPosition.setX(tempX);
-        //nextPosition.setY(tempY);
-        
-        
-        //move(nextMove);
-
-    }
-    */
-
+    /**
+     * Contains all the data relevant to the current position
+     */
     private class PositionStruct{
         public PositionStruct prevPos;
         public Position pos;
         public int depth; 
     }
 
+    /**
+     * Uses Breadth-First Search to move this cat to its
+     * next position that is closer to the mouse
+     * 
+     * @param mousePosition     Mouse's current position on the board
+     */
     public void catchMouse(Position mousePosition) {
         //Acknowledgements: Thanks to Kevin Litvin for the idea and https://www.youtube.com/watch?v=KiCBXu4P-2Y for the explanation :) 
 
@@ -139,33 +76,50 @@ class Cat extends MovingEntity {
         while(possibleMoves.size() > 0){
             int curPosValue;
             PositionStruct curPos = possibleMoves.remove(0);
+
             if (visitedPos.contains(curPos.pos)) { continue; }
+
             visitedPos.add(curPos.pos);
-            if(curPos.depth <= maxDepth){
+
+            if (curPos.depth <= maxDepth){
                 getMovesFromPos(possibleMoves, curPos);
             }
 
             curPosValue = rateNextMoveManhattan(mousePosition, curPos.pos);
-            // We have to move
-            if(curPos.pos != getPos() && (bestMove == null || bestScore > curPosValue )){
+
+            // This ensures that this cat moves at all times
+            if (curPos.pos != getPos() && (bestMove == null || bestScore > curPosValue )){
                 bestScore = curPosValue;
                 bestMove = curPos;
             }
 
             // Stop search early if we hit mouse
-            if(bestScore == 0){
-                Game.State = Game.STATE.LOSE;
-                //break; TODO check if this is necessary
+            if (bestScore == 0){
+                break;
             }
         }
 
-        while(bestMove.depth != 1){
+        while (bestMove.depth != 1){
             bestMove = bestMove.prevPos;
         }
+
         move(bestMove.pos);
+
+        //If the move that the cat takes collides with mouse, end game
+        if (bestMove.pos.getX() == mousePosition.getX() && bestMove.pos.getY() == mousePosition.getY()) {
+            Game.State = Game.STATE.LOSE;
+        }
     }
 
-    //Calculate the possible moves to move to from the current position
+    /**
+     * Scans the next cells in the north, south, east and west
+     * directions to determine which ones this cat can move to 
+     * vs which ones are blocked and adds them to the list of
+     * possible moves
+     * 
+     * @param possibleMoves     ArrayList containing possible positions to move to next
+     * @param curPos            Struct containing all relevant info pertaining to current position
+     */
     private void getMovesFromPos(ArrayList<PositionStruct> possibleMoves, PositionStruct curPos){
         ArrayList<Position> adjacent = new ArrayList<Position>();
         adjacent.add(new Position(curPos.pos.getX()+1, curPos.pos.getY()));
@@ -186,56 +140,21 @@ class Cat extends MovingEntity {
 
     }
 
+    /**
+     * Uses Manhattan distance to give each possible next move a
+     * rating
+     * <p>
+     * Manhattan distance is calculated by the number of cells
+     * needed to reach the target moving only vertically and
+     * horizontally
+     * @param mousePos      current Mouse position
+     * @param potentialMove a valid move the cat can take
+     * @return      rating as an integer
+     */
     private int rateNextMoveManhattan(Position mousePos, Position potentialMove){
         int rating = Math.abs(mousePos.getX() - potentialMove.getX()) + Math.abs(mousePos.getY() - potentialMove.getY());
 
         return rating;
-    }
-
-
-
-
-
-
-    /**
-     * Returns the number of nodes the cat can move to
-     * @param miniX
-     * @param miniY
-     * @param dirX
-     * @param dirY
-     * @param moveX
-     * @param moveY
-     * @param miniMap
-     * @return
-     */
-    private int findNodes(int miniX, int miniY, int[] dirX, int[] dirY, Queue<Integer> moveX, Queue<Integer> moveY, int[][] miniMap) {
-        int nodeCounter = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int tempX = miniX + dirX[i];
-            int tempY = miniY + dirY[i];
-
-            //Check is next move is a barrier
-            if (miniMap[tempX][tempY] == 0) {
-                moveX.add(tempX);
-                moveY.add(tempY);
-                nodeCounter++;
-            }
-
-        }
-
-        return nodeCounter;
-    }
-
-    
-    private Position getBestMoveBFS(Queue<Integer> moveX, Queue<Integer> moveY) {
-    
-        Position nextPosition = new Position(getPos().getX(), getPos().getY());
-
-        nextPosition.setX(moveX.remove());
-        nextPosition.setY(moveY.remove());
-        
-        return nextPosition;
     }
     
 } 
