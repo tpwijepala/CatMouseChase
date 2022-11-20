c7c26670528c2dc018e92fb06c192be7224d47bc
Robert Wilson <rjw11@sfu.ca>
2022-11-20 15:03:56 -0800

    added test functions for checkFinish

--- Documents/src/test/java/MouseTest.java
+++ Documents/src/test/java/MouseTest.java
@@ -1,12 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class MouseTest {
     //for each?
-    Map map = new Map();
+    Map map;
+
+    @BeforeEach
+    public void resetMap() {
+        map = new Map();
+        Game.State = Game.State.GAME;
+    }
 
     @Test
     public void checkAtFinishNotAllCrumbs() {
+
+        map.crumbsCollect = 3;
+        map.player.pos = new Position(map.endX, map.endY);
+        map.player.checkFinish();
+        assertTrue(Game.State == Game.State.GAME);
+    }
+
+    @Test
+    public  void checkAtFinishWithAllCrumbs() {
+        Map map = new Map();
         map.crumbsCollect = 4;
         map.player.pos = new Position(map.endX, map.endY);
         map.player.checkFinish();
@@ -14,12 +31,10 @@ public class MouseTest {
     }
 
     @Test
-    public  void checkAtFinishWithAllCrumbs() {
-
-    }
-
-    @Test
     public void checkNotFinish() {
-
+        Map map = new Map();
+        map.player.pos = new Position(map.endX - 1, map.endY - 1);
+        map.player.checkFinish();
+        assertTrue(Game.State == Game.State.GAME);
     }
 }
