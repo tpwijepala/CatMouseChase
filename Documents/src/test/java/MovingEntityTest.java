c1af5ae278fba6c8937fcd721f2154d97d19a0de
Robert Wilson <rjw11@sfu.ca>
2022-11-20 16:10:20 -0800

    Added tests for checkValidMove()

--- Documents/src/test/java/MovingEntityTest.java
+++ Documents/src/test/java/MovingEntityTest.java
@@ -11,11 +11,11 @@ public class MovingEntityTest {
     public void resetMap() {
         map = new Map();
         Game.State = Game.State.GAME;
+        map.player.pos = pos;
     }
 
     @Test
     public void checkMoveNoChange() {
-        map.player.pos = pos;
         map.player.move(pos);
         // check position remains the same
         assertTrue(map.player.pos.getX() == 4);
@@ -24,7 +24,6 @@ public class MovingEntityTest {
 
     @Test
     public void checkMoveX() {
-        map.player.pos = pos;
         map.player.move(new Position(5, 4));
         // check only X changed
         assertTrue(map.player.pos.getX() == 5);
@@ -33,7 +32,6 @@ public class MovingEntityTest {
 
     @Test
     public void checkMoveY() {
-        map.player.pos = pos;
         map.player.move(new Position(4, 5));
         // check only Y changed
         assertTrue(map.player.pos.getX() == 4);
@@ -42,7 +40,6 @@ public class MovingEntityTest {
 
     @Test
     public void checkMoveXAndY() {
-        map.player.pos = pos;
         map.player.move(new Position(5, 5));
         // check both X and Y changed
         assertTrue(map.player.pos.getX() == 5);
@@ -52,6 +49,31 @@ public class MovingEntityTest {
 
     @Test
     public void moveIntoWall() {
+        // Position (0, 0) contains a wall
+        assertTrue(map.isWall(0, 0) == 1);
+        assertFalse(map.player.checkValidMove(new Position(0, 0)));
+    }
 
+    @Test
+    public void moveOutOfBoundsHigh() {
+        // Corner of map: (57, 41)
+        assertFalse(map.player.checkValidMove(new Position(58, 41)));
+        assertFalse(map.player.checkValidMove(new Position(57, 42)));
+        assertFalse(map.player.checkValidMove(new Position(58, 42)));
+    }
+
+    @Test
+    public void moveOutOfBoundsLow() {
+        // Corner of map: (0, 0)
+        assertFalse(map.player.checkValidMove(new Position(-1, 0)));
+        assertFalse(map.player.checkValidMove(new Position(0, -1)));
+        assertFalse(map.player.checkValidMove(new Position(-1, -1)));
+    }
+
+    @Test
+    public void moveValid() {
+        // Position (5, 5) does not contain a wall
+        assertTrue(map.isWall(5, 5) == 0);
+        assertTrue(map.player.checkValidMove(new Position(5, 5)));
     }
 }
