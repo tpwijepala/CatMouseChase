e32cc644c65b5ef10ad0533a453965cf20c45986
kkramer215 <kkramer215@gmail.com>
2022-11-21 18:49:20 -0800

    Add code coverage report

---
+++ Documents/src/test/java/CatTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeEach;
+
+public class CatTest {
+    Cat testCat = new Cat(12, 34);
+
+    @Test
+    public void checkManhattanRating() {
+        
+    }
+
+}
