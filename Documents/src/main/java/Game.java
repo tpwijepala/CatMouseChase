3e07359929b003cb07ff272a07943657d5a5e4ca
epa25 <epa25@sfu.ca>
2022-11-22 20:05:42 -0800

    Trying to make restart work, some stuff resets,WIP

--- Documents/src/main/java/Game.java
+++ Documents/src/main/java/Game.java
@@ -1,4 +1,6 @@
+//package main.java;
 
+// import java.Map;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
@@ -19,16 +21,20 @@ public class Game extends Canvas implements Runnable {
     public static final int WIDTH = 1450;
     public static final int HEIGHT = 1025;
     public static final int SCALE = 1;
-    public final String TITLE = "CAT AND MOUSE CHASE";
+    public final String TITLE = "PROJECT TEST";
 
     private boolean isPlaying = false;
+    private boolean programRunning = true;
     private Thread thread;
 
     // private BufferedImage image = new BufferedImage(WIDTH, HEIGHT,
     // BufferedImage.TYPE_INT_RGB);
 
     private Menu menu;
-    Map map = new Map();
+    Map map;
+    GameTimer gametimer;
+    Mouse mouse;
+    Score score;
 
     public enum STATE {
         MENU,
@@ -47,8 +53,8 @@ public class Game extends Canvas implements Runnable {
         this.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
         this.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 
-        new Window("WELCOME TO: CAT AND MOUSE CHASE", this);
-
+        new Window("276 Project", this);
+        map = new Map();
         this.addKeyListener(new UserInput(map.getPlayer()));
 
         menu = new Menu();
@@ -99,17 +105,21 @@ public class Game extends Canvas implements Runnable {
 
         }
         if (State == STATE.GAME) {
+            isPlaying = true;
             map.drawEntities(g);
+
         }
-        if (State == STATE.WIN) {
-            menu.win(g);
-        }
+
         if (State == STATE.LOSE) {
+            isPlaying = false;
+            // restart();
             menu.lose(g);
         }
         if (State == STATE.WIN) {
+            // restart();
             menu.win(g);
         }
+
         g.dispose();
         bs.show();
     }
@@ -121,6 +131,17 @@ public class Game extends Canvas implements Runnable {
         // black background
         g.setColor(Color.blue);
         g.fillRect(0, 0, 1450, 1025);
+
+    }
+
+    public void restart() {
+
+        map = new Map();
+        mouse = new Mouse(map.startX, map.startY, map);
+        System.out.println(mouse.getMouseScore());
+        this.addKeyListener(new UserInput(map.getPlayer()));
+        score = new Score();
+        map.generateCats();
 
     }
 
@@ -143,31 +164,40 @@ public class Game extends Canvas implements Runnable {
         double delta = 0;
         long timer = System.currentTimeMillis();
         int frames = 0;
-        while (isPlaying) {
-            long now = System.nanoTime();
-            delta += (now - lastTime) / ns;
-            lastTime = now;
-            if (delta >= 1) {
-
-                delta--;
+        do {
+            if (State == STATE.LOSE || State == STATE.WIN) {
+                restart();
                 draw();
-                frames++;
+            }
+            while (isPlaying) {
+
+                long now = System.nanoTime();
+                delta += (now - lastTime) / ns;
+                lastTime = now;
+                if (delta >= 1) {
+
+                    delta--;
+                    draw();
+                    frames++;
+                }
+
+                if (System.currentTimeMillis() - timer > 1000) {
+                    timer += 1000;
+                    // System.out.println("FPS: " + frames);
+                    frames = 0;
+                }
             }
 
-            if (System.currentTimeMillis() - timer > 1000) {
-                timer += 1000;
-                frames = 0;
-            }
-        }
-
+        } while (programRunning);
         stop();
+
     }
 
     /*
      * Main method, start of the game
      */
     public static void main(String[] args) {
-        System.out.println("Game loading...");
+
         Game game = new Game();
         game.start();
 
--- Documents/src/main/java/Mouse.java
+++ Documents/src/main/java/Mouse.java
@@ -4,40 +4,42 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
-public class Mouse extends MovingEntity{
-    //private Image picture;  // mousePic can be declared const and given an initial value once a picture is found and included in files
-    //private Position mousePosition;
-    //Position pos;
+public class Mouse extends MovingEntity {
+    // private Image picture; // mousePic can be declared const and given an initial
+    // value once a picture is found and included in files
+    // private Position mousePosition;
+    // Position pos;
     Map map;
     private Score playerScore = new Score();
     Position newPos;
 
     public Mouse(int x, int y, Map m) {
-        super(x,y);
+        super(x, y);
         map = m;
         newPos = pos;
-        try{
+        try {
             picture = ImageIO.read(new File("src/main/resources/mouse.png"));
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     public Position getMousePosition() {
         return pos;
     }
 
     public void checkFinish() {
         Position[] end = map.getEnd();
-        //System.out.println("INSIDE checkFinish. X = " + getMousePosition().getX() + " Y = " + getMousePosition().getY());
+        // System.out.println("INSIDE checkFinish. X = " + getMousePosition().getX() + "
+        // Y = " + getMousePosition().getY());
         if (getMousePosition().getX() == end[0].getX()) {
             for (int i = 0; i < end.length; i++) {
                 if (getMousePosition().getY() == end[i].getY()) {
-                    //if (map.crumbsCollect == 4) {
-                        //System.out.println("MOUSE WON");
-                        Game.State = Game.STATE.WIN;
-                    //}
-                    
+                    // if (map.crumbsCollect == 4) {
+                    // System.out.println("MOUSE WON");
+                    Game.State = Game.STATE.WIN;
+                    // }
+
                 }
             }
         }
@@ -47,11 +49,11 @@ public class Mouse extends MovingEntity{
         StaticEntity item = Map.getItem(getPos());
         if (item != null) {
             playerScore.setScore(item.getPoints());
-            //System.out.println(item.getPoints());
-            
+            // System.out.println(item.getPoints());
+
             if (item instanceof Cheese)
                 map.cheeseExist(true);
-            
+
             if (playerScore.checkScoreBelowZero() == true) {
                 Game.State = Game.STATE.LOSE;
             }
@@ -59,7 +61,7 @@ public class Mouse extends MovingEntity{
             else {
                 map.removeItem(item);
             }
-            
+
         }
     }
 
@@ -67,4 +69,7 @@ public class Mouse extends MovingEntity{
         return playerScore;
     }
 
+    public void setMouseScore() {
+        playerScore = new Score();
+    }
 }
